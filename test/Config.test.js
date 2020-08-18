e4926f38268593e2237afbda8ffbb3e6dc7fb427
Karim Mohamed <komicho1996@gmail.com>
2020-08-18 19:02:18 +0200

    Install chai package

--- test/Config.test.js
+++ test/Config.test.js
@@ -1,12 +1,12 @@
-const assert = require('assert');
+const assert = require('chai').assert;
 const Config = require('./../src/config');
 
-describe('Test Config Settings', () => {
+describe('Test Config Settings for endPoint', () => {
     it('Test default endPoint', () => {
         assert.equal(Config.settings.endPoint, 'https://foxlaby.com/api/smslaby/send');
     });
 
-    it('Test default endPoint after the change', () => {
+    it('Test endPoint after the change', () => {
         Config.reSet({
             endPoint: 'https://foxlaby.com/api/v1/smslaby/send'
         });
